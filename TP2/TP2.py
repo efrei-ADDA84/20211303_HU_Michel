da26d3e82c7d7e21b34a4d80017690600e188b75
michou <michel.hu@hotmail.fr>
2023-04-28 17:02:12 +0200

    TP2

---
+++ TP2/TP2.py
@@ -0,0 +1,21 @@
+import os
+import requests
+from fastapi import FastAPI, Request
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def get_weather(request: Request):
+    api_key = os.getenv("API_KEY", "")
+    lat = request.query_params.get('lat')
+    lon = request.query_params.get('lon')
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
+    weather = requests.get(url).json()
+    info_globale = weather["weather"][0]["description"]
+    ville = weather["name"]
+    temperature = weather["main"]["temp"] - 273.15
+    return {"ville": ville, "info_globale": info_globale, "temperature": temperature}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8081)

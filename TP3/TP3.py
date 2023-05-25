1d167bc9e1e0c9a52cd0ef10754bae008eaf70f7
michou <michel.hu@hotmail.fr>
2023-05-25 11:21:18 +0200

    TP3

--- TP3/TP3.py
+++ TP3/TP3.py
@@ -18,4 +18,4 @@ async def get_weather(request: Request):
     return {"ville": ville, "info_globale": info_globale, "temperature": temperature}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=80)
+    uvicorn.run(app, host="0.0.0.0", port=80)
